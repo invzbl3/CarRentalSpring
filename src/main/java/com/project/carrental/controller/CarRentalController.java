ad47161ce1a5828c9db61d5af68d6846ad7eb19a
Maksym Rohachov <romax982@gmail.com>
2021-02-06 23:55:00 +0200

    Updated CarRentalController w/ only 1 method

--- src/main/java/com/project/carrental/controller/CarRentalController.java
+++ src/main/java/com/project/carrental/controller/CarRentalController.java
@@ -1,35 +1,21 @@
 package com.project.carrental.controller;
 
-import com.project.carrental.service.command.*;
 import com.project.carrental.service.factory.CommandFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.Map;
 
 @Controller
 public class CarRentalController {
     @Autowired
     private CommandFactory commandFactory; //singleton object instance injected to be used/shared by all controllers
 
-    @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
-    public String processGetRequest(@RequestParam("command") String command) throws ServletException, IOException { // extract parameter from the request
-        return "index"; // return name of the jsp file to render
-    }
-
-    /*@PostMapping("/post") // this method is called for POST request for /CarRental/post page
-    public String processPostRequest() {
-        return "post"; // would render post.jsp page
-    }*/
-
-    @RequestMapping(value = { "/CarRentalServlet" }, method = RequestMethod.GET)
+    @RequestMapping(value = { "/CarRentalServlet" }, method = { RequestMethod.GET, RequestMethod.POST })
     public String getCommand(@RequestParam("command") String command,
                             HttpServletRequest req, HttpServletResponse res,
                             HttpSession session) throws ServletException, IOException { // extract parameter from the request
