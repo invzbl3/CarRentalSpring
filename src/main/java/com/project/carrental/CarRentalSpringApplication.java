d40a4fa2959f42ae92bae87a974b7e1392f0b539
Maksym Rohachov <romax982@gmail.com>
2021-02-07 03:57:07 +0200

    Updated CarRentalSpringApplication, deleted web.xml

--- src/main/java/com/project/carrental/CarRentalSpringApplication.java
+++ src/main/java/com/project/carrental/CarRentalSpringApplication.java
@@ -13,27 +13,7 @@ import java.util.Map;
 @SpringBootApplication
 public class CarRentalSpringApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CarRentalSpringApplication.class, args);
-	}
-
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(CarRentalSpringApplication.class, ComponentConfiguration.class);
-	}
-
-	@Bean
-	public MeteorServlet dispatcherServlet() {
-		return new MeteorServlet();
-	}
-
-	@Bean
-	public ServletRegistrationBean dispatcherServletRegistration() {
-		ServletRegistrationBean registration = new ServletRegistrationBean(dispatcherServlet());
-		Map<String,String> params = new HashMap<>();
-		params.put("org.atmosphere.servlet","org.springframework.web.servlet.DispatcherServlet");
-		params.put("contextClass","org.springframework.web.context.support.AnnotationConfigWebApplicationContext");
-		params.put("contextConfigLocation","net.org.selector.animals.config.ComponentConfiguration");
-		registration.setInitParameters(params);
-		return registration;
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CarRentalSpringApplication.class, args);
+    }
 }
