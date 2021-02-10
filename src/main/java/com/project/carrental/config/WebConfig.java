e2ae365f5f376b3369d13968018a41cc0471647f
Maksym Rohachov <romax982@gmail.com>
2021-02-10 18:38:56 +0200

    Updated WebConfig

--- src/main/java/com/project/carrental/config/WebConfig.java
+++ src/main/java/com/project/carrental/config/WebConfig.java
@@ -10,6 +10,8 @@ import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import java.util.Locale;
+
 @Configuration
 //@EnableWebMvc
 @ComponentScan
@@ -28,10 +30,21 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     }*/
 
     @Bean
-    public InternalResourceViewResolver jspViewResolver() {
+    public InternalResourceViewResolver jspViewResolver() throws Exception {
         InternalResourceViewResolver resolver= new InternalResourceViewResolver();
         resolver.setPrefix("/WEB-INF/jsp/");
         resolver.setSuffix(".jsp");
+        resolver.setViewNames("admin", "index", "order");
+        return resolver;
+    }
+
+
+    @Bean
+    public InternalResourceViewResolver jspxViewResolver() throws Exception {
+        InternalResourceViewResolver resolver= new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jspx");
+        resolver.setViewNames("error", "info_order", "info_registration", "login", "registration");
         return resolver;
     }
 
