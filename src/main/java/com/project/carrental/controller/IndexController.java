3fb2b41496e6978b9c2289fb096b12872d3c071e
Maksym Rohachov <romax982@gmail.com>
2021-02-13 23:21:14 +0200

    Updated IndexController w/ pagination logic

--- src/main/java/com/project/carrental/controller/IndexController.java
+++ src/main/java/com/project/carrental/controller/IndexController.java
@@ -1,14 +1,21 @@
 package com.project.carrental.controller;
 
+import com.project.carrental.entity.Vehicle;
 import com.project.carrental.repository.OrderRepository;
 import com.project.carrental.repository.VehicleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.stream.IntStream;
 
 @Controller
 public class IndexController {
@@ -28,5 +35,17 @@ public class IndexController {
         modelAndView.addObject("orderList", orderRepository.findAll());
         modelAndView.addObject("vehicleList", vehicleRepository.findAll());
         return modelAndView;
+    }
+
+    @GetMapping("/pagination")
+    public String listVehicles(
+            Model model,
+            @RequestParam(value = "page", required = false, defaultValue = "0") Integer page
+    ) {
+        Page<Vehicle> vehiclePage = vehicleRepository.findAll(new PageRequest(page, 2, new Sort(Sort.Direction.DESC, "Daily Price")));
+        model.addAttribute("vehiclesPage", vehiclePage);
+        model.addAttribute("vehicles", IntStream.range(0, vehiclePage.getTotalPages()).toArray());
+
+        return "index";
     }
 }
