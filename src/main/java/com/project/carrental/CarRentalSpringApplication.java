1823f6c0f2f9afe3240fc8fb37fc8e9eb9ff0d01
Maksym Rohachov <romax982@gmail.com>
2021-02-17 01:44:31 +0200

    Updated CarRentalSpringApplication without BCryptPasswordEncoder

--- src/main/java/com/project/carrental/CarRentalSpringApplication.java
+++ src/main/java/com/project/carrental/CarRentalSpringApplication.java
@@ -10,17 +10,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class CarRentalSpringApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String password = "user";
-        // String encodedPassword = passwordEncoder.encode(password);
-        boolean isPasswordMatches = passwordEncoder.matches(password, "$2a$10$iBEWy3dsU/imowFjEeBfo./8NMVxgaqOLTYdTy8Rkr1z/KFb4v2EO");
-        System.out.println(isPasswordMatches);
-
-        /*System.out.println();
-        System.out.println("Password is         : " + password);
-        System.out.println("Encoded Password is : " + encodedPassword);*/
-
         SpringApplication.run(CarRentalSpringApplication.class, args);
     }
 
